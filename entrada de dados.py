d3f41ac7a077c7ae378606ec7e388b772b2594e9
Haidran <64438131+Haidran@users.noreply.github.com>
2020-04-27 17:22:14 -0300

    Delete entrada de dados.py

--- entrada de dados.py
+++
@@ -1,8 +0,0 @@
-#Exercício para pegar dados do usuário, executar uma ação e imprimir
-
-nome = input("Digite seu nome ")
-idade = input ("digite sua idade ")     # A função input sempre retorna uma string
-ano_nascimento = 2020 - int(idade)
-
-print(f'{nome} tem {idade} anos \n'
-      f'{nome} nasceu em {ano_nascimento}.')
